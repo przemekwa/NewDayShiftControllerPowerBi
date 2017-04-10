21051f5e4db5612b9c76485c096abb47c642eca5
Przemek <przemyslaw.walkowski@bccgroup.com>
2017-04-10 17:07:23 +0200

    Refaktoring kodu

--- src/visual.ts
+++ src/visual.ts
@@ -26,11 +26,6 @@
  *  THE SOFTWARE.
  */
 
-
-
-
-
-
 module powerbi.extensibility.visual {
 
     export interface DataRow {
@@ -46,19 +41,13 @@ module powerbi.extensibility.visual {
         NONE
     }
 
-
     export class Visual implements IVisual {
         private target: HTMLElement;
-        private updateCount: number;
 
         constructor(options: VisualConstructorOptions) {
-
-
-
             console.log('Visual constructor', options);
             this.target = options.element;
             this.target.innerHTML = "";
-            this.updateCount = 0;
 
             jQuery(this.target).ready(() => {
                 console.log("Dokument ready")
@@ -76,91 +65,28 @@ module powerbi.extensibility.visual {
                 allData.push(obj);
             });
 
-            console.log("Działa2?")
-
-
             let groupByDate = this.GroupBy(allData, "Date") as Array<any>;
 
-            let htmlOutput = `<div class="container">
-  <div class="panel-group">
-    <div class="panel panel-default">
-      <div class="panel-heading">Zmiany</div>
-      <div class="panel-body">
-       <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>Data</th>
-        <th>S11</th>
-        <th>S12</th>
-        <th>S14</th>
-        <th>S16</th>
-        <th>S15</th>
-        <th>S21</th>
-        <th>S22</th>
-        <th>S23</th>
-        <th>S01</th>
-        <th>S02</th>
-      </tr>
-    </thead>
-    <tbody>`
-
-            console.log(groupByDate);
+            let htmlOutput = `<div class="container"><div class="panel-group"><div class="panel panel-default"><div class="panel-heading">Zmiany</div><div class="panel-body"><table class="table table-striped"><thead>      <tr>        <th>Data</th>        <th>S11</th>        <th>S12</th>        <th>S14</th>        <th>S16</th>        <th>S15</th>        <th>S21</th>        <th>S22</th>        <th>S23</th>        <th>S01</th>        <th>S02</th>      </tr>    </thead>    <tbody>`
 
             for (var propertyName in groupByDate) {
-
                 var date = new Date(propertyName);
-
                 htmlOutput += "<tr><td>" + this.getFormattedDate(date) + "</td>"
-
-
                 htmlOutput += this.getTdTag(this.CheckTask1Shft(date, groupByDate[propertyName] as Array<DataRow>, "S11"));
-
                 htmlOutput += this.getTdTag(this.CheckTask1Shft(date, groupByDate[propertyName] as Array<DataRow>, "S12"));
                 htmlOutput += this.getTdTag(this.CheckTask1NShft(date, groupByDate[propertyName] as Array<DataRow>, "S14"));
-                htmlOutput +=  this.getTdTag(this.CheckTask1NShft(date, groupByDate[propertyName] as Array<DataRow>, "S16"));
-                htmlOutput +=  this.getTdTag(this.CheckTask1NShft(date, groupByDate[propertyName] as Array<DataRow>, "S15"));
+                htmlOutput += this.getTdTag(this.CheckTask1NShft(date, groupByDate[propertyName] as Array<DataRow>, "S16"));
+                htmlOutput += this.getTdTag(this.CheckTask1NShft(date, groupByDate[propertyName] as Array<DataRow>, "S15"));
                 htmlOutput += this.getTdTag(this.CheckTask1Shft(date, groupByDate[propertyName] as Array<DataRow>, "S21"));
                 htmlOutput += this.getTdTag(this.CheckTask1Shft(date, groupByDate[propertyName] as Array<DataRow>, "S22"));
-                htmlOutput +=  this.getTdTag(this.CheckTask1NShft(date, groupByDate[propertyName] as Array<DataRow>, "S23"));
-                htmlOutput +=  this.getTdTag(this.CheckTaskWeekendShft(date, groupByDate[propertyName] as Array<DataRow>, "S01"));
-                htmlOutput +=  this.getTdTag(this.CheckTaskWeekendShft(date, groupByDate[propertyName] as Array<DataRow>, "S02"));
-
-
-
-
-
-
+                htmlOutput += this.getTdTag(this.CheckTask1NShft(date, groupByDate[propertyName] as Array<DataRow>, "S23"));
+                htmlOutput += this.getTdTag(this.CheckTaskWeekendShft(date, groupByDate[propertyName] as Array<DataRow>, "S01"));
+                htmlOutput += this.getTdTag(this.CheckTaskWeekendShft(date, groupByDate[propertyName] as Array<DataRow>, "S02"));
             }
-
-
-            htmlOutput += `
-     
-  
-     
-    </tbody>
-  </table>
-      </div>
-    </div>
-   
-  </div>
-</div>`
+            htmlOutput += `</tbody></table></div></div></div></div>`
 
             this.target.innerHTML = htmlOutput;
-
-
-
-
-
-
-
-
-            //   console.log(allDate.filter((element)=>element.Day == "21").length)
-            console.log(jQuery().jquery)
-
-
-            console.log(jQuery(this.target).html())
         }
-
 
         private GroupBy(xs: Array<DataRow>, key: any): any {
             return xs.reduce(function (rv, x) {
@@ -169,12 +95,11 @@ module powerbi.extensibility.visual {
             }, {});
         }
 
-
         private getTdTag(result: ShiftResult): string {
 
             switch (result) {
                 case ShiftResult.GOOD:
-                    return '<td><span data-toggle="tooltip" title="Hooray!" class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>'
+                    return '<td><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>'
                 case ShiftResult.DOWN:
                     return '<td><span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></td>'
                 case ShiftResult.UP:
@@ -182,14 +107,13 @@ module powerbi.extensibility.visual {
                 case ShiftResult.NONE:
                     return '<td><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></td>'
             }
-
         }
 
         private CheckTask1Shft(date: Date, rows: Array<DataRow>, task: string): ShiftResult {
 
             if (date.getDay() == 6 || date.getDay() == 0) {
                 return ShiftResult.NONE
-            }   
+            }
 
             switch (rows.filter(e => e.Name == task).length) {
                 case 1:
@@ -201,11 +125,11 @@ module powerbi.extensibility.visual {
             }
         }
 
-           private CheckTaskWeekendShft(date: Date, rows: Array<DataRow>, task: string): ShiftResult {
+        private CheckTaskWeekendShft(date: Date, rows: Array<DataRow>, task: string): ShiftResult {
 
             if (date.getDay() > 0 && date.getDay() < 6) {
                 return ShiftResult.NONE
-            }   
+            }
 
             switch (rows.filter(e => e.Name == task).length) {
                 case 1:
@@ -217,11 +141,11 @@ module powerbi.extensibility.visual {
             }
         }
 
-          private CheckTask1NShft(date: Date, rows: Array<DataRow>, task: string): ShiftResult {
+        private CheckTask1NShft(date: Date, rows: Array<DataRow>, task: string): ShiftResult {
 
             if (date.getDay() == 6 || date.getDay() == 0) {
                 return ShiftResult.NONE
-            }   
+            }
 
             switch (rows.filter(e => e.Name == task).length) {
                 case 0:
@@ -234,9 +158,13 @@ module powerbi.extensibility.visual {
         private getFormattedDate(date) {
             var year = date.getFullYear();
             var month = (1 + date.getMonth()).toString();
+
             month = month.length > 1 ? month : '0' + month;
+
             var day = date.getDate().toString();
+
             day = day.length > 1 ? day : '0' + day;
+
             return day + '-' + month + '-' + year;
         }
     }
