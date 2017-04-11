ca011b3285d27d43fb78b9cd8e69727ac3a100a2
Przemek <przemyslaw.walkowski@bccgroup.com>
2017-04-11 11:44:37 +0200

    Dodanie karuzeli

--- src/visual.ts
+++ src/visual.ts
@@ -24,9 +24,32 @@
  *  THE SOFTWARE.
  */
 
+
+
 module powerbi.extensibility.visual {
 
+    export function logExceptions(): MethodDecorator {
+        return function (target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<Function>)
+        : TypedPropertyDescriptor<Function> {
+            
+            return {
+                value: function () {
+                    try {
+                        return descriptor.value.apply(this, arguments);
+                    } catch (e) {
+                        console.error(e);
+                        throw e;
+                    }
+                }
+            }
+        }
+    }
+     
+    
+     
+     
 
+     
     export interface DataRow {
         Date: Date,
         TaskId: string,
@@ -40,22 +63,32 @@ module powerbi.extensibility.visual {
         NONE
     }
 
+    interface ExtraShiftResult {
+        Result: ShiftResult,
+        UserNames: string
+    }
+
     export class Visual implements IVisual {
         private target: HTMLElement;
+        private polishDay: Array<string> = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"]
+
 
         constructor(options: VisualConstructorOptions) {
             console.log('Visual constructor', options);
             this.target = options.element;
             this.target.innerHTML = "";
 
-         
-
+ 
+ 
             jQuery(this.target).ready(() => {
                 console.log("Dokument ready")
-
             });
         }
 
+       
+    
+
+        @logExceptions() 
         public update(options: VisualUpdateOptions) {
             let allData: Array<DataRow> = [];
             this.target.innerHTML = "";
@@ -66,35 +99,32 @@ module powerbi.extensibility.visual {
                 allData.push(obj);
             });
 
+
             let groupByDate = this.GroupBy(allData, "Date") as Array<any>;
 
-            let htmlOutput = `<div class="container"><div class="panel-group"><div class="panel panel-default"><div class="panel-heading">Zmiany</div><div class="panel-body"><table class="table table-striped tablefont"><thead>      <tr> <th>Dzień</th>        <th>Data</th>        <th>S11</th>        <th>S12</th>        <th>S14</th>        <th>S16</th>        <th>S15</th>        <th>S21</th>        <th>S22</th>        <th>S23</th>        <th>S01</th>        <th>S02</th>      </tr>    </thead>    <tbody>`
+        
+
+let panelArray:Array<string> = [];
 
             for (var propertyName in groupByDate) {
+
+                let panel =  `<div class="panel-group shiftPanel"><div class="panel panel-default"><div class="panel-heading">Zmiany</div><div class="panel-body"><table class="table table-striped tablefont"><thead>      <tr> <th>Dzień</th>        <th>Data</th>        <th>S11</th>        <th>S12</th>        <th>S14</th>        <th>S16</th>        <th>S15</th>        <th>S21</th>        <th>S22</th>        <th>S23</th>        <th>S01</th>        <th>S02</th>      </tr>    </thead>    <tbody>`
+
                 var date = new Date(propertyName);
+                panel += "<tr><td>" + this.polishDay[date.getDay()] + "</td>"
+                panel += "<td>" + this.getFormattedDate(date) + "</td>"
+                panel += this.getTdTag(this.checkExaclyOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S11"));
+                panel += this.getTdTag(this.checkExaclyOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S12"));
+                panel += this.getTdTag(this.checkAtLeastOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S14"));
+                panel += this.getTdTag(this.checkAtLeastOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S16"));
+                panel += this.getTdTag(this.checkAtLeastOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S15"));
+                panel += this.getTdTag(this.checkExaclyOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S21"));
+                panel += this.getTdTag(this.checkExaclyOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S22"));
+                panel += this.getTdTag(this.checkAtLeastOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S23"));
+                panel += this.getTdTag(this.checkkWeekendShft(date, groupByDate[propertyName] as Array<DataRow>, "S01"));
+                panel += this.getTdTag(this.checkkWeekendShft(date, groupByDate[propertyName] as Array<DataRow>, "S02"));
 
-                let polishDay: Array<string> = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"]
-
-
-                htmlOutput += "<tr><td>" + polishDay[date.getDay()] + "</td>"
-                htmlOutput += "<td>" + this.getFormattedDate(date) + "</td>"
-                htmlOutput += this.getTdTag(this.checkExaclyOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S11"));
-                htmlOutput += this.getTdTag(this.checkExaclyOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S12"));
-                htmlOutput += this.getTdTag(this.checkAtLeastOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S14"));
-                htmlOutput += this.getTdTag(this.checkAtLeastOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S16"));
-                htmlOutput += this.getTdTag(this.checkAtLeastOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S15"));
-                htmlOutput += this.getTdTag(this.checkExaclyOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S21"));
-                htmlOutput += this.getTdTag(this.checkExaclyOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S22"));
-                htmlOutput += this.getTdTag(this.checkAtLeastOneShft(date, groupByDate[propertyName] as Array<DataRow>, "S23"));
-                htmlOutput += this.getTdTag(this.checkkWeekendShft(date, groupByDate[propertyName] as Array<DataRow>, "S01"));
-                htmlOutput += this.getTdTag(this.checkkWeekendShft(date, groupByDate[propertyName] as Array<DataRow>, "S02"));
-            }
-
-
-
-
-
-            htmlOutput += `</tbody></table></div></div><div class="panel-group">
+                 panel += `</tbody></table></div></div><div class="panel-group">
     <div class="panel panel-default" style="margin-top: 10px; width: 400px ">
       <div class="panel-heading" style="font-size: 10px">Legenda</div>
       <div class="panel-body">
@@ -117,7 +147,75 @@ module powerbi.extensibility.visual {
       </div>
     </div>
    
-  </div></div></div>`
+  </div></div>`
+
+  panelArray.push(panel);
+
+
+
+            }
+
+            console.log(panelArray.length);
+
+            let htmlOutput = "";    
+
+
+
+
+
+
+htmlOutput = `<div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+ 
+          
+
+`     
+
+panelArray.forEach((element, index) => {
+    
+    if (index === 0) {
+     htmlOutput+=`<div class="item active">`
+    }
+    else
+    {
+ htmlOutput+=`<div class="item">`
+    }
+    
+
+htmlOutput+= element;
+
+
+    htmlOutput+=`</div>`
+
+});
+
+
+ htmlOutput += `   </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" style="color: black" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>`
+
+           
 
             this.target.innerHTML = htmlOutput;
         }
@@ -133,7 +231,7 @@ module powerbi.extensibility.visual {
 
             switch (result) {
                 case ShiftResult.GOOD:
-                    return '<td><span class="glyphicon glyphicon-ok good" aria-hidden="true"></span></td>'
+                    return '<td><a href="#" data-toggle="tooltip" data-placement="top"  data-html="true" title="<em>Tooltip</em> <u>with</u> <b>HTML</b>"><span class="glyphicon glyphicon-ok good" aria-hidden="true" ></span></a></td>'
                 case ShiftResult.DOWN:
                     return '<td><span class="glyphicon glyphicon-arrow-down down" aria-hidden="true"></span></td>'
                 case ShiftResult.UP:
@@ -149,7 +247,9 @@ module powerbi.extensibility.visual {
                 return ShiftResult.NONE
             }
 
-            switch (rows.filter(e => e.Name == task).length) {
+            let result = rows.filter(e => e.Name == task);
+
+            switch (result.length) {
                 case 1:
                     return ShiftResult.GOOD
                 case 0:
@@ -164,6 +264,8 @@ module powerbi.extensibility.visual {
             if (date.getDay() > 0 && date.getDay() < 6) {
                 return ShiftResult.NONE
             }
+
+
 
             switch (rows.filter(e => e.Name == task).length) {
                 case 1:
